15b6a04398a2f112c9027c52a0f9a9ea9b77fe3f
huangkai <huankai@139.com>
2019-01-27 21:10:22 +0800

    修改cas 配置

---
+++ hk-core-authentication-security-cas/src/main/java/com/hk/authentication/security/cas/userdetails/CustomGrantedAuthorityFromAssertionAttributesUserDetailsService.java
@@ -0,0 +1,50 @@
+package com.hk.authentication.security.cas.userdetails;
+
+import com.hk.commons.util.ByteConstants;
+import com.hk.commons.util.CollectionUtils;
+import com.hk.core.authentication.security.SecurityUserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.validation.Assertion;
+import org.springframework.security.cas.userdetails.AbstractCasAssertionUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Map;
+import java.util.Set;
+
+@RequiredArgsConstructor
+public class CustomGrantedAuthorityFromAssertionAttributesUserDetailsService
+        extends AbstractCasAssertionUserDetailsService {
+
+    private final Set<String> defaultRoles;
+
+    private final Set<String> defaultPermissions;
+
+    @Override
+    protected UserDetails loadUserDetails(Assertion assertion) {
+        AttributePrincipal principal = assertion.getPrincipal();
+        Map<String, Object> attributes = assertion.getAttributes();
+        SecurityUserPrincipal userPrincipal = new SecurityUserPrincipal(CollectionUtils.getStringValue(attributes, "id"),
+                principal.getName(),
+                CollectionUtils.getBooleanValue(attributes, "protect_user", false),
+                CollectionUtils.getStringValue(attributes, "real_name"),
+                CollectionUtils.getByteValue(attributes, "user_ype"),
+                CollectionUtils.getStringValue(attributes, "phone"),
+                CollectionUtils.getStringValue(attributes, "email"),
+                CollectionUtils.getByteValue(attributes, "sex"),
+                CollectionUtils.getStringValue(attributes, "icon_path"),
+                null, CollectionUtils.getByteValue(attributes, "user_status", ByteConstants.ONE));
+        userPrincipal.setOrgId(CollectionUtils.getStringValue(attributes, "org_id"));
+        userPrincipal.setOrgName(CollectionUtils.getStringValue(attributes, "org_name"));
+        userPrincipal.setDeptId(CollectionUtils.getStringValue(attributes, "dept_id"));
+        userPrincipal.setDeptName(CollectionUtils.getStringValue(attributes, "dept_name"));
+        if (CollectionUtils.isNotEmpty(defaultRoles)) {
+            userPrincipal.setRoleSet(defaultRoles);
+        }
+        if (CollectionUtils.isNotEmpty(defaultPermissions)) {
+            userPrincipal.setPermissionSet(defaultPermissions);
+        }
+        return userPrincipal;
+    }
+
+}
