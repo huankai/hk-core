e69701315a1b16f0378c7ba0df8c7eb2c048aa92
huangkai <huankai@139.com>
2020-01-09 08:01:22 +0800

    update

--- hk-core-authentication-security-oauth2/src/main/java/com/hk/core/authentication/oauth2/matcher/NoPermitMatcher.java
+++ hk-core-authentication-security-oauth2/src/main/java/com/hk/core/authentication/oauth2/matcher/NoPermitMatcher.java
@@ -1,16 +1,11 @@
 package com.hk.core.authentication.oauth2.matcher;
 
-import com.hk.commons.util.ArrayUtils;
-import com.hk.commons.util.CollectionUtils;
 import com.hk.commons.util.StringUtils;
 import com.hk.core.authentication.api.PermitMatcher;
 import com.hk.core.authentication.oauth2.utils.AccessTokenUtils;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -21,30 +16,28 @@ import java.util.Set;
  */
 public class NoPermitMatcher implements RequestMatcher {
 
-    private final List<RequestMatcher> requestMatchers;
+//    private final List<RequestMatcher> requestMatchers;
 
     public NoPermitMatcher(Set<PermitMatcher> permitMatchers) {
-        this.requestMatchers = new ArrayList<>();
-        if (CollectionUtils.isNotEmpty(permitMatchers)) {
-            RequestMatcher matcher;
-            for (var permitMatcher : permitMatchers) {
-                String[] uris = permitMatcher.getUris();
-                if (ArrayUtils.isNotEmpty(uris)) {
-                    for (var item : uris) {
-                        matcher = new AntPathRequestMatcher(item,
-                                permitMatcher.getMethod() == null ? null : permitMatcher.getMethod().name());
-                        requestMatchers.add(matcher);
-                    }
-
-                }
-            }
-        }
+//        this.requestMatchers = new ArrayList<>();
+//        if (CollectionUtils.isNotEmpty(permitMatchers)) {
+//            RequestMatcher matcher;
+//            for (var permitMatcher : permitMatchers) {
+//                String[] uris = permitMatcher.getUris();
+//                if (ArrayUtils.isNotEmpty(uris)) {
+//                    for (var item : uris) {
+//                        matcher = new AntPathRequestMatcher(item,
+//                                permitMatcher.getMethod() == null ? null : permitMatcher.getMethod().name());
+//                        requestMatchers.add(matcher);
+//                    }
+//
+//                }
+//            }
+//        }
     }
 
     @Override
     public boolean matches(HttpServletRequest request) {
-        var accessToken = AccessTokenUtils.getAccessToken(request);
-        return StringUtils.isNotEmpty(accessToken)
-                && requestMatchers.stream().anyMatch(item -> item.matches(request));
+        return StringUtils.isEmpty(AccessTokenUtils.getAccessToken(request));
     }
 }
